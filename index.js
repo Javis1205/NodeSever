7735ab7d64208df21c3d8677b5e2ba127d549999
JavisNguyen <dai12051997@gmail.com>
2018-09-04 16:26:05 +0700

    no message

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 // define a simple route
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
     res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
+});
+
+// listen for requests
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
 });
