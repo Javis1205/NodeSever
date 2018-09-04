89ee1e6f9fb25c9bf40dd04eb43b9b6e97b6347b
JavisNguyen <dai12051997@gmail.com>
2018-09-04 17:05:14 +0700

    no message

---
+++ config/database.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url: 'mongodb+srv://root:Daiyeulinh1205!@javisdata-dcc3b.gcp.mongodb.net/javis?retryWrites=true'
+}
--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+const Router = require('./app/routes/note.routes');
+mongoose.Promise = global.Promise;
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...');
+    process.exit();
+});
+
 // create express app
 const app = express();
 
@@ -14,7 +27,7 @@ app.use(bodyParser.json())
 app.get('/api', (req, res) => {
     res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
 });
-
+Router(app);
 // listen for requests
 app.listen(PORT, () => {
     console.log("Server is start");
